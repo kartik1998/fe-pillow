fdd5cdaea3760d89d9f9bbec473f955b69d92453
kartik1998 <kartikrawat61@gmail.com>
2021-09-21 16:33:19 +0530

    setup material ui album layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Landing from "./containers/Landing";
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
 }
 
 export default App;
