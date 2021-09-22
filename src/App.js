ade576597e25fe65bedef376991e7ca600b83bb4
kartik1998 <kartikrawat61@gmail.com>
2021-09-23 00:03:47 +0530

    Setup auth context demo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Landing from './containers/Landing';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import { useContext } from 'react';
+import { firebaseAuth } from './context/AuthContext';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 function App() {
+  const { test } = useContext(firebaseAuth);
+  console.log({ test });
   return (
     <Router>
       <Route exact path="/" component={Landing} />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AuthContext from './context/AuthContext';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <AuthContext>
     <App />
-  </React.StrictMode>,
+  </AuthContext>,
   document.getElementById('root'),
 );
