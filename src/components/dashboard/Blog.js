8b3ffcd00e607b8acde2761fb6292b58d74a294c
kartik1998 <kartikrawat61@gmail.com>
2021-09-25 00:52:05 +0530

    Add: Pllow wallet ui

--- src/components/dashboard/Blog.js
+++ src/components/dashboard/Blog.js
@@ -12,9 +12,6 @@ import FeaturedPost from './FeaturedPost';
 import Main from './Main';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
-import post1 from './blog-post.1.md';
-import post2 from './blog-post.2.md';
-import post3 from './blog-post.3.md';
 
 const mainFeaturedPost = {
   title: 'Overview',
@@ -37,32 +34,6 @@ const featuredPosts = [
   },
 ];
 
-const posts = [post1, post2, post3];
-
-const sidebar = {
-  title: 'About',
-  description:
-    'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
-  archives: [
-    { title: 'March 2020', url: '#' },
-    { title: 'February 2020', url: '#' },
-    { title: 'January 2020', url: '#' },
-    { title: 'November 1999', url: '#' },
-    { title: 'October 1999', url: '#' },
-    { title: 'September 1999', url: '#' },
-    { title: 'August 1999', url: '#' },
-    { title: 'July 1999', url: '#' },
-    { title: 'June 1999', url: '#' },
-    { title: 'May 1999', url: '#' },
-    { title: 'April 1999', url: '#' },
-  ],
-  social: [
-    { name: 'GitHub', icon: GitHubIcon },
-    { name: 'Twitter', icon: TwitterIcon },
-    { name: 'Facebook', icon: FacebookIcon },
-  ],
-};
-
 const theme = createTheme({
   shadows: ['none'],
 });
@@ -82,17 +53,10 @@ export default function Blog() {
           </Grid>
           <Grid container spacing={5} sx={{ mt: 3 }}>
             <Main title="Your Investments" />
-            <Sidebar
-              title={sidebar.title}
-              description={sidebar.description}
-              archives={sidebar.archives}
-              social={sidebar.social}
-            />
+            <Sidebar />
           </Grid>
         </main>
       </Container>
-      <br />
-      <br />
       <Footer />
     </ThemeProvider>
   );
--- src/components/dashboard/Sidebar.js
+++ src/components/dashboard/Sidebar.js
@@ -1,12 +1,9 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 const pillowFont = {
   fontSize: '18px',
@@ -17,21 +14,30 @@ const pillowFont = {
   fontWeight: 'bold',
 };
 
-const usdtFont = {
-  fontSize: '26px',
+const valueFont = {
+  fontSize: '21px',
   fontFamily: `'Graphik Web Medium', sans-serif;`,
   fontStyle: 'normal',
   fontStretch: 'normal',
-  color: '#ADD8E6',
+  color: '#2f363f',
 };
 
-function Sidebar(props) {
-  const { archives, description, social, title } = props;
+const descFont = {
+  fontFamily: `'Graphik Web Medium', sans-serif;`,
+  color: '#81878B',
+  fontWeight: 'bold',
+};
 
+const boxSx = {
+  flex: 1,
+  pl: '140px',
+};
+
+function Sidebar() {
   return (
     <Grid item xs={12} md={4}>
       <Paper variant="outlined">
-        <Typography align="center" style={pillowFont} sx={{ paddingBottom: '50px', paddingTop: '7px' }}>
+        <Typography align="center" style={pillowFont} sx={{ paddingBottom: '30px', paddingTop: '7px' }}>
           Pillow Wallet{' '}
           <img
             src="https://cdn.iconscout.com/icon/premium/png-256-thumb/bitcoin-wallet-3058908-2546445.png"
@@ -39,26 +45,34 @@ function Sidebar(props) {
             style={{ paddingTop: '12px' }}
           />
         </Typography>
+        <Grid container sx={{ pl: '60px', pb: '30px' }}>
+          <Grid item md={6}>
+            <Typography style={descFont} gutterBottom>
+              USDT Balance
+            </Typography>
+            <Typography variant="p" style={valueFont} paragraph sx={{ pl: '20px' }}>
+              $0.00
+            </Typography>
+          </Grid>
+          <Grid item md={6}>
+            <Typography style={descFont} gutterBottom>
+              USDC Balance
+            </Typography>
+            <Typography variant="p" style={valueFont} paragraph sx={{ pl: '20px' }}>
+              $0.00
+            </Typography>
+          </Grid>
+        </Grid>
+        <Box sx={{ pb: '20px', pl: '120px' }}>
+          <Button variant="contained">Invest More</Button>
+        </Box>
+
+        <Box sx={{ pb: '20px', pl: '105px' }}>
+          <Button variant="outlined">Withdraw Funds</Button>
+        </Box>
       </Paper>
     </Grid>
   );
 }
-
-Sidebar.propTypes = {
-  archives: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      url: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
-  description: PropTypes.string.isRequired,
-  social: PropTypes.arrayOf(
-    PropTypes.shape({
-      icon: PropTypes.elementType.isRequired,
-      name: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
-  title: PropTypes.string.isRequired,
-};
 
 export default Sidebar;
