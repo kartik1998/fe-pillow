7920b22a87f943a1c8305fc4e8a965b6aaf0f5d7
kartik1998 <kartikrawat61@gmail.com>
2021-09-24 23:01:31 +0530

    Add: securitas paper

--- src/components/dashboard/Main.js
+++ src/components/dashboard/Main.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import Markdown from './Markdown';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 const titleFont = {
   fontSize: '17px',
@@ -15,15 +17,94 @@ const titleFont = {
   paddingBottom: '8px',
 };
 
+const overviewFont = {
+  fontSize: '20px',
+  fontFamily: `'Graphik Web Medium', sans-serif;`,
+  fontStyle: 'normal',
+  fontStretch: 'normal',
+  fontWeight: '550',
+  color: '#2f363f',
+};
+
+const valueFont = {
+  fontSize: '21px',
+  fontFamily: `'Graphik Web Medium', sans-serif;`,
+  fontStyle: 'normal',
+  fontStretch: 'normal',
+  color: '#2f363f',
+};
+
+const descFont = {
+  fontFamily: `'Graphik Web Medium', sans-serif;`,
+  color: '#81878B',
+};
+
+const boxSx = {
+  position: 'relative',
+  p: { xs: 3, md: 2 },
+  pr: { md: 0 },
+};
+
 function Main(props) {
-  const { posts, title } = props;
+  const { title } = props;
 
   return (
     <Grid item xs={12} md={8}>
       <Typography style={titleFont} gutterBottom>
         {title}
       </Typography>
-      <Divider />
+      <Divider /> <br />
+      <Paper>
+        <Grid container>
+          <Grid item xs={12} sm={6} md={2}>
+            <Box sx={boxSx}>
+              <img
+                src="https://thumbs.dreamstime.com/b/black-chess-knight-background-chessboard-cells-figure-horse-symbol-concept-design-realistic-vector-illustration-147739836.jpg"
+                alt="securitas"
+                height="100px"
+                width="70px"
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <Box sx={boxSx}>
+              <Typography variant="p" style={overviewFont} paragraph>
+                Securitas
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <Box sx={boxSx}>
+              <Typography variant="p" style={descFont} gutterBottom>
+                Investment
+              </Typography>
+              <Typography variant="p" style={valueFont} paragraph>
+                $0.00
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <Box sx={boxSx}>
+              <Typography variant="p" style={descFont} gutterBottom>
+                Returns
+              </Typography>
+              <Typography variant="p" style={valueFont} paragraph>
+                $0.00
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <Box sx={boxSx}>
+              <Button variant="outlined">Invest </Button>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <Box sx={boxSx}>
+              <Button variant="outlined">Redeem</Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Paper>
     </Grid>
   );
 }
